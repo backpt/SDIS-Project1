1834e3d3cb918332e394f25e4491773ea1d381fe
nice5torm <marianadguimaraes@hotmail.com>
2018-03-23 00:33:19 +0000

    identifier :dancer:

--- src/protocols/Backup.java
+++ src/protocols/Backup.java
@@ -3,6 +3,7 @@ package protocols;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.security.MessageDigest;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import main.*;
@@ -29,6 +30,32 @@ public class Backup {
 
 	private void createIdentifier() {
 		// TODO Auto-generated method stub
+		try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            FileInputStream fis = new FileInputStream(this.fileName);
+           
+            byte[] dataBytes = new byte[8192];
+ 
+            int nread = 0;
+            while ((nread = fis.read(dataBytes)) != -1) {
+              md.update(dataBytes, 0, nread);
+            };
+            byte[] mdbytes = md.digest();
+            fis.close();
+ 
+ 
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < mdbytes.length; i++) {
+              sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+           
+            this.fileID = sb.toString();
+            // System.out.println("SHA: " + this.hash);
+ 
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 		
 	}
 
